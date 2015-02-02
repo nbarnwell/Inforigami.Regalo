fbe7ac2fa7420ff3a0f652a802f1fce9df367a7e
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2015-02-02 17:50:05 +0000

    Referenced assemblyinfo.version.cs from interfaces project in all other packaged projects.

--- Inforigami.Regalo.Interfaces/Properties/AssemblyInfo.cs
+++ Inforigami.Regalo.Interfaces/Properties/AssemblyInfo.cs
@@ -7,7 +7,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Inforigami.Regalo.Interfaces")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Inforigami.Regalo.Interfaces")]
@@ -15,16 +14,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
