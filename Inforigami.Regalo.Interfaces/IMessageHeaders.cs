f899a4106f96a82844322cbd60091570c157c64a
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2016-01-27 16:04:30 +0000

    Added timestamp to message contract.

--- Inforigami.Regalo.Interfaces/IMessageHeaders.cs
+++ Inforigami.Regalo.Interfaces/IMessageHeaders.cs
@@ -6,6 +6,7 @@ namespace Inforigami.Regalo.Interfaces
     public interface IMessageHeaders
     {
         Guid MessageId { get; set; }
+        DateTimeOffset Timestamp { get; set; }
         Dictionary<string, string> CustomHeaders { get; set; }
     }
 }
--- Inforigami.Regalo.Interfaces/MessageHeaders.cs
+++ Inforigami.Regalo.Interfaces/MessageHeaders.cs
@@ -6,11 +6,13 @@ namespace Inforigami.Regalo.Interfaces
     public abstract class MessageHeaders : IMessageHeaders
     {
         public Guid MessageId { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
         public Dictionary<string, string> CustomHeaders { get; set; }
 
         protected MessageHeaders()
         {
             MessageId = Guid.NewGuid();
+            Timestamp = DateTimeOffset.Now;
             CustomHeaders = new Dictionary<string, string>();
         }
     }
