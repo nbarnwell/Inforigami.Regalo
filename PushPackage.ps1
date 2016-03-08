f59754610498dad3ad9a569529db569ade601208
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2016-03-08 12:31:37 +0000

    Updated package push script to support parameters for source and symbols exclusion.

--- PushPackage.ps1
+++ PushPackage.ps1
@@ -1,4 +1,9 @@
-write-verbose "Preparing to push packages to nuget..."
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(
+    [string] $Source,
+    [switch] $IncludeSymbols)
+
+write-verbose "Preparing to push packages..."
 
 $scriptDir = $MyInvocation.MyCommand.Path | split-path
 $outputDir = "$scriptDir\BuildOutput"
@@ -6,7 +11,23 @@ write-debug "outputDir = $outputDir"
 
 md $outputDir -f | out-null
 
-gci $outputDir -filter *.nupkg | %{ 
+$exclude = $null;
+
+if ($IncludeSymbols -eq $false) {
+    write-verbose "Excluding symbols"
+    $exclude = "*.symbols.*";
+}
+
+gci "$outputDir\*" -filter *.nupkg -exc $exclude | %{ 
     Write-Host -ForegroundColor Green "$_"
-    nuget.exe push $_.FullName
+
+    if ($Source) {
+        if ($PSCmdLet.ShouldProcess("Push $($_.Fullname) to $Source")) {
+            nuget.exe push $_.FullName -Source $Source
+        }
+    } else {
+        if ($PSCmdLet.ShouldProcess("Push $($_.Fullname) to default source")) {
+            nuget.exe push $_.FullName
+        }
+    }
 }
