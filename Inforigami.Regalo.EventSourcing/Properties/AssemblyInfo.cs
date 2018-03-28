28b22b5b813733c9b2aaf780593463fc8a22a26e
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2018-03-29 00:34:04 +0100

    Ensure assemblyinfo files are included so version on binary is correct.

--- Inforigami.Regalo.EventSourcing/Properties/AssemblyInfo.cs
+++ Inforigami.Regalo.EventSourcing/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Inforigami.Regalo.EventSourcing")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Inforigami.Regalo.EventSourcing")]
