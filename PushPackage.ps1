623fa18eaf3589ba1f32b0c2904f58441b605cd9
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2022-11-07 23:24:38 +0000

    Tidy solution folder

--- PushPackage.ps1
+++ PushPackage.ps1
@@ -1,33 +1,19 @@
 [CmdletBinding(SupportsShouldProcess = $true)]
-param(
-    [string] $Source,
-    [switch] $IncludeSymbols)
+param()
 
 write-verbose "Preparing to push packages..."
 
 $scriptDir = $MyInvocation.MyCommand.Path | split-path
-$outputDir = "$scriptDir\BuildOutput"
+$outputDir = "$scriptDir\nugets"
 write-debug "outputDir = $outputDir"
 
 md $outputDir -f | out-null
 
-$exclude = $null;
+gci "$outputDir\*" -filter *.nupkg | 
+    Foreach-Object { 
+        Write-Host -ForegroundColor Green "$_"
 
-if ($IncludeSymbols -eq $false) {
-    write-verbose "Excluding symbols"
-    $exclude = "*.symbols.*";
-}
-
-gci "$outputDir\*" -filter *.nupkg -exc $exclude | %{ 
-    Write-Host -ForegroundColor Green "$_"
-
-    if ($Source) {
-        if ($PSCmdLet.ShouldProcess("Push $($_.Fullname) to $Source")) {
-            nuget.exe push $_.FullName -Source $Source
-        }
-    } else {
         if ($PSCmdLet.ShouldProcess("Push $($_.Fullname) to default source")) {
-            nuget.exe push $_.FullName
+            dotnet nuget push $_.FullName
         }
     }
-}
